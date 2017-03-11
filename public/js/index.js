7a486e967ea5fc5c3f7cc7a9aebf4c5413306ddd
Robert van der Mark <robert.vandermark@gmail.com>
2017-03-11 16:19:00 -0500

    add all items

--- public/js/index.js
+++ public/js/index.js
@@ -22,12 +22,74 @@ $(document).ready(function(){
 
 	$("button.btn-primary").on('click',function(){
 		var sel = $(this).prev();
-		//var val = sel.val();
+		var iType = sel.data('type');
 		var text = sel.find(':selected').text();
 		var id = sel.find(':selected').val()
-		console.log('text and value',text,id)
+
+		itineraryAdd(iType, id, 0);	
 
 	})
 
 });
+
+var itineraryAdd = function(type, id, day){
+	var oneDay = itineraries[day]
+	if (oneDay[type].indexOf(id) < 0){
+			oneDay[type].push(id)
+	}
+	renderItinerary(day);
+};
+
+var renderItinerary = function(day){
+	var html = '<div class="itinerary-item"> \
+       		<span class="title">TESTAndaz Wall Street</span> \
+            <button class="btn btn-xs btn-danger remove btn-circle">x</button> \
+            </div>'
+
+    var todayItinerary = itineraries[day]
+    console.log(itineraries[day])
+
+    var hotelsContainer = $('#itinerary').find('.list-group')[0];
+    var restaurantsContainer = $('#itinerary').find('.list-group')[1];
+    var activitiesContainer = $('#itinerary').find('.list-group')[2];
+    /*
+    //hotels rendering
+    $(hotelsContainer).empty();
+
+    renderedContent = todayItinerary['hotel'].map(function(hotel){
+    		var hotelObj = hotels.filter(function(item){
+    			return item.id===hotel*1
+    		})[0];
+    		return '<div class="itinerary-item"><span class="title">' + 
+    		hotelObj.name
+    		+ '</span></div>'
+    	}).join('')
+
+    $(hotelsContainer).append(renderedContent);*/
+
+    renderContainer(hotelsContainer,hotels, 'hotel',0);
+	renderContainer(restaurantsContainer,restaurants, 'restaurant',0);
+	renderContainer(activitiesContainer,activities, 'activity',0);
+    
+
+
+
+    //console.log('test', hotels)
+}
+
+function renderContainer(container, arr, type, day){
+	$(container).empty();
+
+	todayItinerary = itineraries[day]
+    renderedContent = todayItinerary[type].map(function(id){
+    		var Obj = arr.filter(function(item){
+    			return item.id===id*1
+    		})[0];
+    		return '<div class="itinerary-item"><span class="title">' + 
+    		Obj.name
+    		+ '</span></div>'
+    	}).join('')
+
+    $(container).append(renderedContent);
+}
 
